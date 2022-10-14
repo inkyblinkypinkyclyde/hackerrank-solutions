3c188420568b58a281d1e46c13ae5dfccf32496d
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-10-14 19:05:39 +0100

    added more solutions

--- python/nestedList.py
+++ python/nestedList.py
@@ -1,25 +1,17 @@
 if __name__ == '__main__':
     all_students = []
+    all_scores = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
         all_students.append([name, score])
-    # all_students = sorted(all_students, key=lambda x: x[1], reverse=True) # Descending sorted by score
-    # second_lowest_score = all_students[-2][1] # second_to_last element in list
-    all_scores = []
-    for student in all_students:
-        if student[1] not in all_scores:
-            all_scores.append(student[1])
-    print(all_scores)
-    sorted_scores = sorted(all_scores)
-    second_lowest_score = all_scores[-2] # second_to_last element in list
-    print(second_lowest_score)
+        if score not in all_scores:
+            all_scores.append(score)
+    sorted_scores = all_scores.sort()
     second_lowest_students = []
     for student in all_students:
-        if student[1] == second_lowest_score:
+        if student[1] == all_scores[1]:
             second_lowest_students.append(student[0])
-    print(second_lowest_students)
-    for student in sorted(second_lowest_students): # sort the names alphabetically
-        print(student) # print each name on new line
-    # sort the names alphabetically
+    for student in sorted(second_lowest_students):
+        print(student)
     
